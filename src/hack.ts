3417c3075b3baf45b32f53531458a31720509c6c
Matt Soto <ThePsychicMuffin@gmail.com>
2023-09-08 19:16:32 -0400

    initial commit based on typescript-template with a few of my first basic scripts

---
+++ src/hack.ts
@@ -0,0 +1,17 @@
+import { NS } from "@ns";
+
+export async function main(ns: NS) {
+  const host: string = <string>ns.args[0];
+  const targetSecurityLevel = ns.getServerMinSecurityLevel(host) + 2;
+  const targetMoneyAvailable = ns.getServerMaxMoney(host) * .9;
+
+  while(true) {
+    if (ns.getServerSecurityLevel(host) > targetSecurityLevel) {
+      await ns.weaken(host);
+    } else if (ns.getServerMoneyAvailable(host) < targetMoneyAvailable) {
+      await ns.grow(host);
+    } else {
+      await ns.hack(host);
+    }
+  }
+}
