0d1eaec60f9aa62d5ebbdefc789f4e0d300eb8ff
Matt Soto <ThePsychicMuffin@gmail.com>
2023-09-12 18:33:17 -0400

    Fix infiltrate bugs

--- src/scripts/check.ts
+++ src/scripts/check.ts
@@ -2,10 +2,11 @@ import {NS} from "@ns";
 
 export async function main(ns: NS) {
   const host: string = <string>ns.args[0];
+  const currentMoney = format(ns, ns.getServerMoneyAvailable(host));
   const maxMoney = format(ns, ns.getServerMaxMoney(host));
   const minSecurity = format(ns, ns.getServerMinSecurityLevel(host));
   const reqHacking = format(ns, ns.getServerRequiredHackingLevel(host));
-  ns.tprint(`Max Money: ${maxMoney}\n`)
+  ns.tprint(`Money: ${currentMoney} / ${maxMoney}\n`)
   ns.tprint(`Min Security: ${minSecurity}\n`)
   ns.tprint(`Req Hacking: ${reqHacking}\n`)
 }
